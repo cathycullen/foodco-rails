7571d950e2a3979bca38b3ac3d19dbc38860fdb4
cathycullen <cathy@softwareoptions.com>
2023-11-10 13:53:51 -0600

    add react directory and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(
+  name: "Pepperoni Pizza",
+  description: "Delicious Pie",
+  image_descr: "image",
+  qty_available: 10,
+  ingredients: "gluten, sauce, mozzarella cheese, pepperoni",
+  original_price: 20,
+  discounted_price: 5,
+  item_category: "meal",
+  suprise: false,
+  rating: "****"
+)
+
+Product.create(
+  name: "Surprise Bag",
+  description: "Assortment of Goodies",
+  image_descr: "image",
+  qty_available: 15,
+  ingredients: "This suprise bag will most likely contain rice, some proteinand a sauce",
+  original_price: 15,
+  discounted_price: 3.99,
+  item_category: "meal",
+  suprise: true,
+  rating: "****",
+)
