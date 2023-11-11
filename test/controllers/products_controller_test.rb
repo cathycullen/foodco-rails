a822932048b8c2794a8b72cd9951e1d79fda2698
cathycullen <cathy@softwareoptions.com>
2023-11-10 21:36:35 -0600

    add associations from products to store

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -12,7 +12,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, discounted_price: @product.discounted_price, image_descr: @product.image_descr, ingredients: @product.ingredients, item_category: @product.item_category, name: @product.name, original_price: @product.original_price, pickkup_time: @product.pickkup_time, pickup_date: @product.pickup_date, qty_available: @product.qty_available, rating: @product.rating, suprise: @product.suprise } }, as: :json
+      post products_url, params: { product: { description: @product.description, discounted_price: @product.discounted_price, image_descr: @product.image_descr, ingredients: @product.ingredients, item_category: @product.item_category, name: @product.name, original_price: @product.original_price, pickkup_time: @product.pickkup_time, pickup_date: @product.pickup_date, qty_available: @product.qty_available, rating: @product.rating, store_id: @product.store_id, suprise: @product.suprise } }, as: :json
     end
 
     assert_response :created
@@ -24,7 +24,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, discounted_price: @product.discounted_price, image_descr: @product.image_descr, ingredients: @product.ingredients, item_category: @product.item_category, name: @product.name, original_price: @product.original_price, pickkup_time: @product.pickkup_time, pickup_date: @product.pickup_date, qty_available: @product.qty_available, rating: @product.rating, suprise: @product.suprise } }, as: :json
+    patch product_url(@product), params: { product: { description: @product.description, discounted_price: @product.discounted_price, image_descr: @product.image_descr, ingredients: @product.ingredients, item_category: @product.item_category, name: @product.name, original_price: @product.original_price, pickkup_time: @product.pickkup_time, pickup_date: @product.pickup_date, qty_available: @product.qty_available, rating: @product.rating, store_id: @product.store_id, suprise: @product.suprise } }, as: :json
     assert_response :success
   end
 
